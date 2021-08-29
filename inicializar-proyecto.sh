e9f5d30d0fbf19f903fd934a66284fda098bc3f4
Iván López <porral.spam@gmail.com>
2021-08-29 10:01:37 +0200

    cambios posibles
    no se si es bueno no no pero es comodo.

--- inicializar-proyecto.sh
+++ inicializar-proyecto.sh
@@ -23,7 +23,11 @@ if [ -n "$1" ]; then # Comprobamos que se le pasa el nombre del poruyecto
 	cp dev.py webappdjango/$1/settings/dev.py  # Esto tendra que ser cambiado a un mv cuando este terminado   
 	cp pro.py webappdjango/$1/settings/pro.py  # Esto tendra que ser cambiado a un mv cuando este terminado   
 	echo "${CVERDE} Añadimos las variables de entorno minimas al .env${NOCOLOR}"
+	#para crear el host desde variables de entorno
+	echo "ALLOWED_HOSTS=*" >> .env
 	echo "DJANGO_SETTINGS_MODULE=${1}.settings.dev" >> .env
+	#¿esto no esta al reves, y suponiendo qu tienes la carpeta settings/dev. -- por defecto sería sin el .dev
+
 	echo "MYSQL_ROOT_PASSWORD=cambiame" >> .env  # Clave root de mariadb
 	echo "MARIADB_DATABASE=${1}" >> .env  # Nombre de la db. Por defecto le pone la del prouecto
 	echo "MARIADB_USER=${1}" >> .env  # Por defecto usa el nombre de prouecto como usuario
